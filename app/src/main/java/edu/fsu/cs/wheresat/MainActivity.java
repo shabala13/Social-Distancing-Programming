cf93c5d37742925480bc3874ddf9c819cbfad0af
Joseph Zilonka <zilonkaj@hotmail.com>
2020-03-31 16:54:50 -0400

    Start Firebase integration

--- app/src/main/java/edu/fsu/cs/wheresat/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/wheresat/MainActivity.java
@@ -3,12 +3,35 @@ package edu.fsu.cs.wheresat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseauth = FirebaseAuth.getInstance();
+    FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText user = findViewById(R.id.username);
+        EditText pass = findViewById(R.id.password);
+
+        if (firebaseUser == null)
+            firebaseauth.signInWithEmailAndPassword(user.getText().toString(), pass.getText().toString());
+
+        Log.d("email", firebaseUser.getEmail());
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        firebaseUser = firebaseauth.getCurrentUser();
     }
 }
