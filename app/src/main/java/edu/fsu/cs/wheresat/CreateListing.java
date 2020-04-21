10af8c73c4b6733c8c9b81c834c8c12408f23e10
KylePeiman <60237268+KylePeiman@users.noreply.github.com>
2020-04-21 14:26:59 -0400

    adding camera code to the create listing page

--- app/src/main/java/edu/fsu/cs/wheresat/CreateListing.java
+++ app/src/main/java/edu/fsu/cs/wheresat/CreateListing.java
@@ -1,14 +1,23 @@
 package edu.fsu.cs.wheresat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class CreateListing extends AppCompatActivity {
+    ImageView imageView = (ImageView) findViewById(R.id.picture);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +25,7 @@ public class CreateListing extends AppCompatActivity {
         setContentView(R.layout.activity_create_listing);
 
         Button login = findViewById(R.id.submitButton);
+        Button open = (Button) findViewById(R.id.openCamera);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,5 +37,28 @@ public class CreateListing extends AppCompatActivity {
             }
         });
 
+
+        if(ContextCompat.checkSelfPermission(CreateListing.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(CreateListing.this, new String[]
+                    {Manifest.permission.CAMERA},100);
+        }
+
+        open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 }
