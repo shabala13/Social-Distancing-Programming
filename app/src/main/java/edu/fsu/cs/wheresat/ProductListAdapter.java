d4e781decf98ddc66bc13171732625116862bbb7
shabala13 <42969167+shabala13@users.noreply.github.com>
2020-04-23 00:37:21 -0400

    Updates search count on an item

--- app/src/main/java/edu/fsu/cs/wheresat/ProductListAdapter.java
+++ app/src/main/java/edu/fsu/cs/wheresat/ProductListAdapter.java
@@ -1,7 +1,6 @@
 package edu.fsu.cs.wheresat;
 import java.util.List;
 import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
