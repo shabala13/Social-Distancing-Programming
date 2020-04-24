b4dc5499ed8178cceaa2c0e26c543fca4f293550
Nodeak <kaedon.hamm@gmail.com>
2020-04-24 16:16:35 -0400

    Fix Mark as Sold to remove item from db. Minor textColor fixes.

--- app/src/main/java/edu/fsu/cs/wheresat/ProductPageActivity.java
+++ app/src/main/java/edu/fsu/cs/wheresat/ProductPageActivity.java
@@ -3,15 +3,29 @@ package edu.fsu.cs.wheresat;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.utilities.Pair;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ProductPageActivity extends AppCompatActivity {
 
@@ -20,8 +34,8 @@ public class ProductPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.product_page);
 
-        String p_store = getIntent().getStringExtra("store");
-        String image_url = getIntent().getStringExtra("image_url");
+        final String p_store = getIntent().getStringExtra("store");
+        final String image_url = getIntent().getStringExtra("image_url");
         final String lat = getIntent().getStringExtra("lat");
         final String lon = getIntent().getStringExtra("lon");
         final String p_title = getIntent().getStringExtra("title");
@@ -53,6 +67,40 @@ public class ProductPageActivity extends AppCompatActivity {
                 Intent i = new Intent(getBaseContext(), MapsActivity.class);
                 i.putExtra("product", "Toilet Paper");
                 startActivity(i); */
+
+                final FirebaseDatabase database = FirebaseDatabase.getInstance();
+                final DatabaseReference itemsRef = database.getReference("Item").child(Utilities.toUpperCase(p_title)).child("entries");
+
+                itemsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        // get HashMap from DB
+                        GenericTypeIndicator<HashMap<String, ProductEntry>> genericTypeIndicator = new GenericTypeIndicator<HashMap<String, ProductEntry>>() {};
+                        HashMap<String, ProductEntry> productEntryHashMap = dataSnapshot.getValue(genericTypeIndicator);
+
+                        for(HashMap.Entry<String, ProductEntry> entry : productEntryHashMap.entrySet()){
+                            ProductEntry productEntry = entry.getValue();
+                            if (image_url.equals(productEntry.image) &&
+                                lat.equals(productEntry.lat) &&
+                                lon.equals(productEntry.lon) &&
+                                p_store.equals(productEntry.store)){
+                                Log.i("NERD", entry.getKey());
+                                DatabaseReference sold_item = itemsRef.child(entry.getKey());
+                                sold_item.removeValue();
+
+
+                            }
+                        }
+
+                    }
+
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
             }
         });
     }
